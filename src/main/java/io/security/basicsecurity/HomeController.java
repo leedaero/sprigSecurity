80fd2436f2e923d576ff6869078ca1d462cc65af
tony_compay <tony@snaps.com>
2024-01-04 22:25:28 +0900

    feat : git 머지 테스트

---
+++ src/main/java/io/security/basicsecurity/HomeController.java
@@ -0,0 +1,16 @@
+package io.security.basicsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/home")
+    public String index(){
+        return  "home";
+    }
+    @GetMapping("/home/main")
+    public String loginPage(){
+        return "loginPage";
+    }
+}
